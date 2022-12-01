d176aea3b43eca00a00c648ce0258ecd3988be7d
sebtrn <77442193+sebtrn@users.noreply.github.com>
2022-12-02 00:42:00 +0100

    jslogin et register

--- script.js
+++ script.js
@@ -3,16 +3,3 @@ bouton.addEventListener("click", function(){
     console.log("Test");
 });
 
-var LoginForm = document.getElementById("LoginForm");
-var RegForm = document.getElementById("RegForm");
-var Indicator = document.getElementById("Indicator");
-
-function register(){
-    RegForm.style.transform = "translateX(0px)";
-    LoginForm.style.transform = "translateX(0px)";
-}   
-
-function login(){
-    RegForm.style.transform = "translateX(300px)";
-    LoginForm.style.transform = "translateX(300px)";
-}
