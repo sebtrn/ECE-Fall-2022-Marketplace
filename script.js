c37c36423398db1af198bd0c06343d8ffe8ae8f6
sebtrn <77442193+sebtrn@users.noreply.github.com>
2022-12-02 00:37:59 +0100

    Formulaire connection

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ var bouton=document.getElementById("test");
 bouton.addEventListener("click", function(){
     console.log("Test");
 });
+
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var Indicator = document.getElementById("Indicator");
+
+function register(){
+    RegForm.style.transform = "translateX(0px)";
+    LoginForm.style.transform = "translateX(0px)";
+}   
+
+function login(){
+    RegForm.style.transform = "translateX(300px)";
+    LoginForm.style.transform = "translateX(300px)";
+}
