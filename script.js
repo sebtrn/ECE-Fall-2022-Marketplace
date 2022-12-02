a79c84fea92987b169aa961cd151db4fec29fe7d
sebtrn <77442193+sebtrn@users.noreply.github.com>
2022-12-02 14:38:51 +0100

    Lien fichier javascript

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-var bouton=document.getElementById("test");
-bouton.addEventListener("click", function(){
-    console.log("Test");
-});
+// var bouton = document.getElementById("test");
+// bouton.addEventListener("click", function () {
+//     console.log("Test");
+// });
 
+
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var Indicator = document.getElementById("Indicator");
+
+function register() {
+    RegForm.style.transform = "translateX(0px)";
+    LoginForm.style.transform = "translateX(0px)";
+    Indicator.style.transform = "translateX(100px)";
+}
+
+function login() {
+    RegForm.style.transform = "translateX(300px)";
+    LoginForm.style.transform = "translateX(300px)";
+    Indicator.style.transform = "translateX(0px)";
+}
